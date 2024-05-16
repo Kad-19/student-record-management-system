e806a22339301fd8dc04360de926723d7fb5343c
Kad-19 <kidusasebe1919@gmail.com>
2024-05-16 12:08:33 +0300

    remove files

--- script/resetPassword.js
+++
@@ -1,9 +0,0 @@
-const emailElement = document.querySelector('#email');
-const resetForm = document.querySelector("form");
-resetForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  if(emailElement.value != ""){
-      window.location.href = "login.html";
-
-  }
-});
