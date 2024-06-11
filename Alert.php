3bae7732a68be775283373f7464b17e70a605370
Kad-19 <kidusasebe1919@gmail.com>
2024-06-11 16:17:58 +0300

    alert box

---
+++ Alert.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alert</title>
+    <link rel="stylesheet" href="./styles/alert.css" />
+</head>
+<body>
+<?php
+echo "<section id='alert-section' class='warning invisible'>
+        <div id='popup'>
+          <div id='alert-title'></div>
+          <div id='alert-description'></div>
+          <button id='close-btn'>Ok</button>
+        </div>
+      </section>";
+function show_alert($title, $description) {
+    echo "<script type='text/javascript'>
+        function showAlert(title, description) {
+            document.getElementById('alert-title').innerText = title;
+            document.getElementById('alert-description').innerText = description;
+            document.getElementById('alert-section').classList.remove('invisible');
+        }
+        showAlert('$title', '$description');
+        const closeBtn = document.getElementById('close-btn');
+        closeBtn.addEventListener('click', () => {
+            document.getElementById('alert-section').classList.add('invisible');
+        });
+    </script>";
+}
+?>
+    
+</body>
+</html>
